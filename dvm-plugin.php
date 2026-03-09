bd0378fc21335ae7137396e6fc767c2bd0659bf4
rasibweb <rasibreigns@gmail.com>
2026-03-09 06:51:29 +0600

    initial commit

---
+++ dvm-plugin.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Plugin Name: DVM Plugin
+ * Description: A plugin to demonstrate Devomodo WordPress plugin structure.
+ * Plugin URI:        https://github.com/rasibweb/dvm-plugin/
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Devomodo
+ * Author URI:        https://devomodo.com/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://devomodo.com/my-plugin/
+ * Text Domain:       dvm-plugin
+ * Domain Path:       /languages
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
