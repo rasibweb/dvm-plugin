c0a664cbb7ca89b2352507a1447517c255529990
rasibweb <rasibreigns@gmail.com>
2026-03-14 06:53:27 +0600

    project meta shortcode

--- inc/shortcode.php
+++ inc/shortcode.php
@@ -24,3 +24,62 @@ function dvm_parameters_shortcode($attr = array()) {
     return $html;
 } 
 add_shortcode('DVM_PARAMETERS', 'dvm_parameters_shortcode');
+
+
+
+function dvm_magicwp_shortcode($atts) {
+    // Extract and merge attributes with defaults
+    if (!empty($atts)) {
+        $atts = shortcode_atts(array(
+            'label' => 'Button Label',
+            'link' => 'http://localhost/testwp/'
+        ), $atts, 'DVM_MAGICWP');
+    } else {
+        $atts = NULL;
+    }
+
+    // Start output buffering
+    ob_start();
+
+    // Your shortcode logic here
+    
+    echo '<div class="dvm-magicwp-shortcode">';
+        if (!empty($atts)) {
+            echo '<a href="' . esc_url($atts['link']) . '" class="dvm-magicwp-button">' . esc_html($atts['label']) . '</a>';
+        } else {
+            echo '<p>No attributes provided. Please add link and label to the shortcode.</p>';
+        }
+    echo '</div>';
+    
+
+    // Return the buffered content
+    return ob_get_clean();
+}
+add_shortcode('DVM_MAGICWP', 'dvm_magicwp_shortcode');
+
+// Usage example:
+// [DVM_MAGICWP attr="value"]
+
+/**
+ * Project Meta Shortcode
+ */
+
+function dvm_project_meta_shortcode($atts){
+
+    $atts = shortcode_atts(array(
+        'id' => get_the_ID(),
+        'link' => 'http://localhost/testwp/'
+    ), $atts, 'DVM_PROJECT_META');
+
+    $project_url = get_post_meta($atts['id'], 'project_url', true);
+    $project_completion = get_post_meta($atts['id'], 'project_completion_duration', true);
+    $project_cost = get_post_meta($atts['id'], 'project_estimated_cost', true);
+    
+    $html = '<div class="dvm-project-meta">';
+        $html .= '<span>'. esc_html($project_url) . '</span><br>';
+        $html .= '<span>'. esc_html($project_completion) . '</span><br>';
+        $html .= '<span>'. esc_html($project_cost) . '</span><br>';
+    $html .= '</div>';
+    return $html;
+}
+add_shortcode('DVM_PROJECT_META', 'dvm_project_meta_shortcode');
